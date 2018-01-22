3f8d4379ac8d2752c18185684cb74d3b58c46737
edwin rivera <waehake@gmail.com>
2018-01-22 16:35:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,6 @@
+
+//post comment from the form
+function postComment() {
+  var commenter = document.getElementById('commenterName').value;
+  var comment = document.getElementById('commentText').value;
+}
