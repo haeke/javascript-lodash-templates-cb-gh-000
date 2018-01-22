4f0f48d1d3ae524fb68e80b7c1241d2aa4fa1823
edwin rivera <waehake@gmail.com>
2018-01-22 16:49:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ function postComment() {
   var commenter = document.getElementById('commenterName').value;
   var comment = document.getElementById('commentText').value;
   
-  var commentTemplate = '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
+  // var commentTemplate = '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
+  var commentTemplate = document.getElementById("comment-template").innerHTML;
   
   var templateFn = _.template(commentTemplate);
   
